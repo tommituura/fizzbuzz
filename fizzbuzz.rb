381840ddfea81c6ef6e1ae2e588a22aefe1cbdac
tommituura <tommi.tuura@gmail.com>
2015-04-07 11:49:39 +0300

    Trimming Ruby version further
    Using all the syntactic sugar at my disposal with ruby here.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/ruby1.9.1
 
-fizzbuzzes = Range.new(1,100).map do |v|
+(1..100).each do |v|
     r = ""
     r += "Fizz" if v % 3 == 0
     r += "Buzz" if v % 5 == 0
     r = v if r.length == 0
-    r
+    puts r
 end
-
-puts fizzbuzzes
